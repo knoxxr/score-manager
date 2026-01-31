fc378664cca515d8c0fc0f436c7f66215930ecc6
knoxxr <knoxxr@gmail.com>
2026-01-31 12:29:34 +0900

    feat: Seed teachers with an initial assignment and include teacher assignments in user authentication for grade lookup.

--- start_server.sh
+++ start_server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Environment setup for Node/NPM
-export PATH=$PATH:/opt/homebrew/bin:/usr/local/bin
+export PATH=$PATH:/home/dashinaru/.nvm/versions/node/v24.13.0/bin
 
 # Navigate to project directory
-cd /Users/smic/dashinaru/score-manager
+cd /home/dashinaru/Git/score-manager
 
 # Start the application in production mode
 # We use 'exec' to replace the shell process with the node process, 
 # ensuring signals are passed correctly.
-exec /opt/homebrew/bin/npm start
+exec npm start
